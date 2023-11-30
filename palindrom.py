1fe03fe07475b1cfe700272b280a6e0c71bbcbbc
riflubis4 <arifwinanda3340@gmail.com>
2023-11-30 23:06:41 +0700

    riflubis4

---
+++ palindrom.py
@@ -0,0 +1,20 @@
+def is_palindrome(kata):
+    N = len(kata)
+    for i in range(1, N + 1):
+        if kata[i - 1] != kata[N - i]:
+            return False
+    return True
+
+while True:
+    N = int(input("Masukkan panjang string: "))
+    kata = input("Masukkan string: ")
+
+    if is_palindrome(kata):
+        print(f"{kata} adalah palindrom.")
+    else:
+        print(f"{kata} Bukan palindrom.")
+
+#if is_palindrome(kata):
+    #print("True")
+#else:
+    #print("False")
