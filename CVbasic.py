449fa0b89c92495b21f3416f4f09027d7a2dd25c
riflubis4 <arifwinanda3340@gmail.com>
2023-12-01 20:01:04 +0700

    pertemuan-4.py

---
+++ CVbasic.py
@@ -0,0 +1,24 @@
+#import library opencv
+
+import cv2
+
+def main(capture):
+    while True:
+        #print(capture.read())
+        #baca kamera
+        ret, frame = capture.read() 
+
+        cv2.imshow('Frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    frame.release()
+    cv2.destroyAllWindows()
+
+if __name__ =='__main__':
+    camera = cv2.VideoCapture(0)
+    main(camera)
+
+
+
